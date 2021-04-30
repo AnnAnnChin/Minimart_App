b6a368925adecba51875c192aba496707b876e7e
AnnAnnChin <annannchin@gmail.com>
2021-04-30 22:56:23 +0800

    Add files via upload

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -19,6 +19,9 @@ function App() {
     }, []);
 	
 	function showFormItemCreate() {
+		document.getElementById("overlay").style.visibility = 'visible';
+		document.getElementById("form_label").innerHTML = "Create Item";
+		
 		document.getElementById("input_id").value = new Date().getTime();
 		document.getElementById("input_name").value = "";
 		document.getElementById("input_price").value = "";
@@ -28,6 +31,9 @@ function App() {
 	}
 	
 	function showFormItemEdit(item) {
+		document.getElementById("overlay").style.visibility = 'visible';
+		document.getElementById("form_label").innerHTML = "Edit Item";
+		
 		document.getElementById("input_id").value = item.id;
 		document.getElementById("input_name").value = item.name;
 		document.getElementById("input_price").value = item.price;
@@ -37,6 +43,7 @@ function App() {
 	}
 	
 	function hideForm() {
+		document.getElementById("overlay").style.visibility = 'hidden';
 	}
 	
 	function createItem() {
@@ -87,88 +94,92 @@ function App() {
 		}
 	}
 	
-  return (
-    <div className="App">
-		<h1 align="center">Minimart</h1>
-		
-		<table align="center">
-			<thead>
-				<tr>
-					<td width="600" align="left">
-						Please refer to the pricelist below to place your orders.
-					</td>
-					<th width="100">
-						<button onClick={() => showFormItemCreate()}>
-							New Item
-						</button>
-					</th>
-				</tr>
-			</thead>
-		</table>
-		
-		<table align="center" border="1">
-			<thead>
-				<tr>
-					<th width="150">ID</th>
-					<th width="250">Name</th>
-					<th width="100">Price</th>
-					<th width="100"></th>
-					<th width="100"></th>
-				</tr>
-			</thead>
-			<tbody>
-			{
-				data.map((item) => (
-					<tr key={item.id} align="center">
-						<td>{item.id}</td>
-						<td>{item.name}</td>
-						<td>${item.price.toFixed(2)}</td>
-						<td>
-							<button onClick={() => showFormItemEdit(item)}>
-								Edit Item
-							</button>
-						</td>
-						<td>
-							<button onClick={() => deleteItem(item)}>
-								Delete Item
-							</button>
-						</td>
-					</tr>
-				))
-			}
-			</tbody>
-		</table>
-		
-		<div id="form" className="overlay">
-			<h1 id="form_label" align="center"></h1>
+	return (
+		<div className="App">
+			<div>
+				<h1 align="center">Minimart</h1>
+				
+				<table align="center">
+					<thead>
+						<tr>
+							<td width="600" align="left">
+								Please refer to the pricelist below to place your orders.
+							</td>
+							<th width="100">
+								<button onClick={() => showFormItemCreate()}>
+									New Item
+								</button>
+							</th>
+						</tr>
+					</thead>
+				</table>
+				
+				<table align="center" border="1">
+					<thead>
+						<tr>
+							<th width="150">ID</th>
+							<th width="250">Name</th>
+							<th width="100">Price</th>
+							<th width="100"></th>
+							<th width="100"></th>
+						</tr>
+					</thead>
+					<tbody>
+					{
+						data.map((item) => (
+							<tr key={item.id} align="center">
+								<td>{item.id}</td>
+								<td>{item.name}</td>
+								<td>${item.price.toFixed(2)}</td>
+								<td>
+									<button onClick={() => showFormItemEdit(item)}>
+										Edit Item
+									</button>
+								</td>
+								<td>
+									<button onClick={() => deleteItem(item)}>
+										Delete Item
+									</button>
+								</td>
+							</tr>
+						))
+					}
+					</tbody>
+				</table>
+			</div>
 			
-			<table align="center">
-				<tbody>
-					<tr>
-						<td width="50">ID</td>
-						<td><input id="input_id" type="number" readOnly="readonly"/></td>
-					</tr>
+			<div id="overlay" className="overlay" onClick={() => hideForm()}>
+				<div id="form" className="form" align="center">
+					<h1 id="form_label" align="center"></h1>
 					
-					<tr>
-						<td width="50">Name</td>
-						<td><input id="input_name" type="text"/></td>
-					</tr>
-					
-					<tr>
-						<td width="50">Price</td>
-						<td><input id="input_price" type="number" step="0.01" min="0" onKeyPress="return ((event.charCode >= 48 && event.charCode <= 57) ||  event.charCode == 46 || event.charCode == 0); "/></td>
-					</tr>
-					
-					<tr>
-						<td colSpan="2" align="center">
-							<button id="form_button"/>
-						</td>
-					</tr>
-				</tbody>
-			</table>
+					<table align="center">
+						<tbody>
+							<tr>
+								<td width="50">ID</td>
+								<td><input id="input_id" type="number" readOnly="readonly"/></td>
+							</tr>
+							
+							<tr>
+								<td width="50">Name</td>
+								<td><input id="input_name" type="text"/></td>
+							</tr>
+							
+							<tr>
+								<td width="50">Price</td>
+								<td><input id="input_price"/></td>
+							</tr>
+							
+							<tr>
+								<td colSpan="2" align="center">
+									<button id="form_button"/>
+								</td>
+							</tr>
+						</tbody>
+					</table>
+				</div>
+			</div>
 		</div>
-	</div>
-  );
+	);
 }
 
 export default App;
